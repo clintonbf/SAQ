7fcefaa4df6e370e5febeecf3f302761e2583902
clintonbf <cfernandes24@my.bcit.ca>
2019-11-21 09:17:16 -0800

    Started the process of modularizing the build functions (chairs and sales), so that they can be used on the fly.

--- SAQ_db_functions.js
+++ SAQ_db_functions.js
@@ -31,3 +31,70 @@ function delete_chair_record(db_ref, chair){
         console.error("Whoops " + error);
     });
 }
+
+//Code for building the Chairs collection
+// Insert a chair
+function insert_chair(add_chair, id){
+    the_db.collection("Chairs").doc("chair_" + id).set({
+        id: id,
+        name: add_chair.name,
+        price: add_chair.price,
+        firmness: add_chair.comfort_options.firmness,
+        back_support: add_chair.comfort_options.back_support,
+        arms: add_chair.comfort_options.arms,
+        ratings: add_chair.ratings,
+        picture: add_chair.picture
+    })
+        .then(function() {
+            console.log("Added chair " + add_chair.name);
+        })
+        .catch(function(error) {
+            console.log("Error adding chair: " + error);
+        });
+}
+
+//Implement insert_chair for all chairs
+function insert_chairs() {
+    let chair_array = [];
+    let chair_path = "images/chair_images/";
+    chair_array.push(new Chair('chair_01', "the Clint", 525, [3, true, false], [1, 2, 1, 5, 5, 5, 4], chair_path + 'the_clint.jpg'));
+    chair_array.push(new Chair(2, "the Fahad", 800, [1, true, true], [3, 3, 3], chair_path + 'the_fahad.jpg'));
+    chair_array.push(new Chair(3, "the Em", 1250, [6, true, true], [5, 5], chair_path + 'the_em.jpg'));
+    chair_array.push(new Chair(4, "the Neda", 3200, [6, true, true], [5], chair_path + 'the_neda.jpg'));
+    chair_array.push(new Chair(5, "the Sam", 800, [9, false, false], [3, 5], chair_path + 'the_sam.jpg'));
+    chair_array.push(new Chair(6, "the Chris", 1000, [10, true, true], [1, 2, 3, 4, 5], chair_path + 'the_chris.jpg'));
+
+    for (let i = 0; i < chair_array.length; i++) {
+        insert_chair(chair_array[i], i);
+    }
+}
+
+//Code for building the Sales collection
+//Insert a sale
+function insert_sale(sale) {
+    the_db.collection("Sales").doc("sale_" + sale.transaction_id).set({
+        transaction_id: sale.transaction_id,
+        chair: sale.chair,
+        sold_to: sale.buyer,
+        sale_date: sale.sale_date
+    })
+        .then(function () {
+            console.log("Added sale " + sale.transaction_id);
+        })
+        .catch(function(error) {
+            console.log("Error adding sale: " + error);
+        });
+}
+
+// Implement insert_sale() for all sales
+function insert_sales() {
+    let sale_array = [];
+    sale_array.push(new Sale(generate_trans_id(), "the Clint", "Boogie Woogie", "Oct 14, 2019"));
+    sale_array.push(new Sale(generate_trans_id(), "the Clint", "GitMaster", "Oct 15, 2019"));
+    sale_array.push(new Sale(generate_trans_id(), "the Neda", "Neda", "Oct 31, 2019"));
+    sale_array.push(new Sale(generate_trans_id(), "the Neda", "Sam", "Nov 31, 2019"));
+
+    for (let j = 0; j < sale_array.length; j++) {
+        insert_sale(sale_array[j]);
+    }
+}
