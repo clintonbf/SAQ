61fca2fd8ed153b7b9fa3f8ceaa0ee552d9d9ea2
Clinton <clintonf@gmail.com>
2019-12-01 16:10:17 -0800

    firebase_config.js code moved into SAQ_db_functions (again, bloody imports).
    Commenting removed in build_sales to allow connection to firestore

--- SAQ_db_functions.js
+++ SAQ_db_functions.js
@@ -1,3 +1,40 @@
+//                                      FIREBASE/FIRESTORE OBJECTS
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCcaat9ekOpu0m7qZTJ6gblV65DIBeWEbQ",
+    authDomain: "saq-fb.firebaseapp.com",
+    databaseURL: "https://saq-fb.firebaseio.com",
+    projectId: "saq-fb",
+    storageBucket: "saq-fb.appspot.com",
+    messagingSenderId: "351007329675",
+    appId: "1:351007329675:web:241d7fd4d85e789830b38e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var the_db = firebase.firestore();
+// Initialize Authentication
+var uiConfig = {
+    signInSuccessUrl: 'main.html',
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    tosUrl: '<your-tos-url>',
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', uiConfig);
+// store user from authentication
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        localStorage.setItem('userName', JSON.stringify(user.displayName));
+    }
+});
+
+
+//                                     OBJECTS
 function Chair(id, name, price, comfort_options, ratings, picture) {
     this.id = id;
     this.name = name;
